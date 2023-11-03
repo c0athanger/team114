48e6b8c09ca564656672247cb8acaa89d05a2d71
mbelinga777 <belingam@oregonstate.edu>
2023-11-02 18:55:28 -0700

    added exercises table

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Exercise from './Exercise'
 
 const Exercises = () => {
 	const exercises_query = [
@@ -8,8 +9,37 @@ const Exercises = () => {
 
 	]
 	return (
-		<div>Exercises</div>
-	)
+		<div>
+		<button>Create New Exercise</button>
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				{exercises_query.map((exercise, index) => (
+					<tr key={index}>
+						<td>{exercise.Name}</td>
+						<td>{exercise.Description}</td>
+						<td>
+							<button>Update</button>
+							<button>Delete</button>
+						</td>
+					</tr>
+				))}
+				<tr>
+					<td><input type="text" placeholder="Exercise Name"/></td>
+					<td><input type="text" placeholder="Exercise Description"/></td>
+					<td><button>Add</button></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	
+);
 }
 
 export default Exercises
