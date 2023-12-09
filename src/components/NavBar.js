8f552cecf0998bc2520f6841f4e59bfa447fa46d
c0athanger <jhill044@uottawa.ca>
2023-12-08 21:19:57 -0500

    added UserWorkouts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,6 +26,7 @@ const NavBar = () => {
 					<li><Link to="/Exercises">Exercises</Link></li>
 					<li><Link to="/Workouts">Workouts</Link></li>
 					<li><Link to="/Bodyparts">Bodyparts</Link></li>
+					<li><Link to="/UserWorkouts">UserWorkouts</Link></li>
 				</ul>
 			</nav>
 		</header>
